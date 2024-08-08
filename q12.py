d9c5e7323877c5aa9df9de3f4cabb44f84e9fa0c
RafaelAlves-maker <fael360alves@gmail.com>
2024-08-08 17:07:10 -0300

    Create q12.py

---
+++ q12.py
@@ -0,0 +1,7 @@
+termo1 = int(input("Digite o primeiro termo: "))
+quantidade = int(input("Digite a quantidade de termos: "))
+razao = int(input("Digite a razão: "))
+y = 0
+for i in range(termo1, quantidade+1, razao):
+  y =+ i
+print(y)
